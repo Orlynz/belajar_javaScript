b7eb580c3100f36f50ab136caaf27fc88346424c
Secondta <idnssquad300@gmail.com>
2022-01-26 15:24:30 +0700

    JavaScript

---
+++ library.js
@@ -0,0 +1,11 @@
+function nama() {
+  console.log("Orlynz");
+}
+
+nama();
+
+document.write("<ol>");
+for (let i = 0; i < laptop.length; i++) {
+  document.write(`<li>${laptop[i]}`);
+}
+document.write("</ol>");
